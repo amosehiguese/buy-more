efcbaed6b6f197a5f417ef26e468c8f41f964732
amosehiguese <amosehiguese@gmail.com>
2023-09-08 13:28:25 +0100

    Modify product model

--- models/product.go
+++ models/product.go
@@ -8,29 +8,29 @@ import (
 )
 
 type Category struct {
-	Uuid 			uuid.UUID			`db:"uuid" validate:"required,uuid"`
+	ID			uuid.UUID			`db:"id" validate:"required,uuid"`
 	Name 			string 				`db:"name" validate:"required,lte=200"`
-	Slug			string				`db:"slug" validate:"required,lte=200"`
+	Slug			string				`db:"slug" validate:"lte=200"`
 	Products		[]Product			`db:"products"`
 	CreatedAt		time.Time			`db:"created_at"`
 	UpdatedAt		time.Time			`db:"updated_at"`
 }
 
 type Product struct{
-	Uuid  			uuid.UUID			`db:"uuid" validate:"required,uuid"`
-	Category		Category			`db:"category" validate:"required"`
+	ID  			uuid.UUID			`db:"id" validate:"required,uuid"`
+	Categories		[]Category			`db:"categories" validate:"required"`
 	Name			string 				`db:"name"  validate:"required,lte200"`
-	Slug			string				`db:"slug" validate:"required,lte=200"`
+	Slug			string				`db:"slug" validate:"lte=200"`
 	Images			[]Image				`db:"images"`
 	Description		string				`db:"description"`
 	Price			decimal.Decimal		`db:"price" validate:"gte=0"`
-	Available		bool				`db:"available"`
+	Available		bool				`db:"available" validate:"required"`
 	CreatedAt		time.Time			`db:"created_at"`
 	UpdatedAt		time.Time			`db:"updated_at"`
 }
 
 type Image struct {
-	Uuid			uuid.UUID		`db:"uuid" validate:"required,uuid"`
+	ID			uuid.UUID		`db:"id" validate:"required,uuid"`
 	ProductID		uuid.UUID 		`db:"product_id" validate:"require,uuid"`
 	Path			string			`db:"path"`
 	CreatedAt		time.Time		`db:"created_at"`
